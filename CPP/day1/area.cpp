b71e1f88095869a244b6a2e45dc2149d3b8b0aff
milinddasarwarcmaug26 <milind.dasarwar.cmaug26@gmail.com>
2026-08-24 22:54:33 +0530

    Add files via upload

---
+++ CPP/day1/area.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+int main(){
+    // area of rectangle =length * width
+    int length;
+    int width;
+    int area;
+    std::cout<<"enter length:\n";
+    std::cin>>length;
+    std::cout<<"enter width:\n";
+    std::cin>>width;
+    area=length*width;
+    std::cout<<"area of rectangle= "<<area;
+
+
+    return 0;
+}
